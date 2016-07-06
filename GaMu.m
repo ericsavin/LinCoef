325eabe195347a783e57e4a733e1a7c9970f9e02
ericsavin <eric.savin@onera.fr>
2016-07-06 14:58:04 +0200

    Create GaMu.m

---
+++ GaMu.m
@@ -0,0 +1,13 @@
+function [gamu] = GaMu(n,alpha)
+
+% Evaluate a special function for the linearization coefficients
+% of Hermite polynomials
+%
+% ESavin / April 2016 / eric.savin@onera.fr
+%
+
+ip   = mod(n,2);
+m    = floor(n/2);
+gamu = power(2,n).*factorial(m).*pochhammer(alpha+0.5,m+ip);
+
+return;
