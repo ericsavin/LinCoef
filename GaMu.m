662a8d31870e889daa798681c783c47a1dba68c3
ericsavin <eric.savin@onera.fr>
2016-07-06 15:46:51 +0200

    Compute a special function

--- GaMu.m
+++ GaMu.m
@@ -1,5 +1,4 @@
 function [gamu] = GaMu(n,alpha)
-
 % Evaluate a special function for the linearization coefficients
 % of Hermite polynomials
 %
@@ -11,3 +10,4 @@ m    = floor(n/2);
 gamu = power(2,n).*factorial(m).*pochhammer(alpha+0.5,m+ip);
 
 return;
+
